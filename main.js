316c3d38664331eb2f53469edec994b0c94ffe06
Seungwoo Choe <seungwoochoes@gmail.com>
2022-03-14 10:49:53 +0900

    Make code purune superflous whitespace before right square brackets and add a label.

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const linebreak = "<br>"; // Depricated Option. Adjust linebreak between lines o
 
 const partOfSpeech = ["adverb", "verb", "pronoun", "noun", "adjective", "preposition", "conjunction", "exclamation"];
 const extraInformation = ["PHRASES", "PHRASAL VERBS", "DERIVATIVES", "ORIGIN"];
-const labels = ["Biology", "technical", "litarary", "Chemistry", "Mathematics", "Geology", "Prosody", "Heraldry", "humorous", "Theology", "historical", "Philosophy", "&", "Scottish informal", "mainly North American", "North American", "Northern English", "mainly British", "British", "Logic", "Grammar", "informal,", "informal", "formal", "Baseball", "Physics", "Golf", "archaic", "US", "Computing", "Printing", "Law", "Anatomy", "Zoology", "rare", "Architecture", "Electronics", "Military", "Photography"];
+const labels = ["Biology", "technical", "litarary", "Chemistry", "Mathematics", "Geology", "Prosody", "Heraldry", "humorous", "Theology", "historical", "Philosophy", "&", "Scottish informal", "mainly North American", "North American,", "North American", "Northern English", "mainly British", "British", "Scottish,", "Scottish", "Logic", "Grammar", "informal,", "informal", "formal", "Baseball", "Physics", "Golf", "archaic", "US", "Computing", "Printing", "Law", "Anatomy", "Zoology", "rare", "Architecture", "Electronics", "Military", "Photography"];
 // Label order e.g.: put "informal" before "formal".
 
 function run(input, parameters) {
@@ -119,6 +119,7 @@ const formatText = (text) => {
 	text = formatByNumbers(text);
 	text = formatByPartOfSpeech(text);
 	text = breakLineProperly(text);
+	text = deleteWhiteSpaceBeforeSquareBrackets(text);
 	if (htmlFormatting === true) {
 		text = formatByHtml(text);
 	}
@@ -152,6 +153,10 @@ const breakLineProperly = (text) => {
 		text = text.replace(regexWithoutSquareBrackets, `$1 $2${linebreak}`);
 	})
 	return text;
+}
+
+const deleteWhiteSpaceBeforeSquareBrackets = (text) => {
+	return text.replace(" ]", "]")
 }
 
 const formatByHtml = (text) => {
